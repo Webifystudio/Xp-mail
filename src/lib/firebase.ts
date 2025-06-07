7632fa16e29d100e1c6572da333ff66ee54fa22e
Webify <studiowebifyofficial@gmail.com>
2025-06-07 12:29:53 +0000

    Use this firebase const firebaseConfig = {     apiKey: "AIzaSyBNyk5Qh-qp

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,14 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "YOUR_API_KEY",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "YOUR_AUTH_DOMAIN",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "YOUR_PROJECT_ID",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "YOUR_STORAGE_BUCKET",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "YOUR_MESSAGING_SENDER_ID",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "YOUR_APP_ID",
-};
+    apiKey: "AIzaSyBNyk5Qh-qpSCHBkKIsoOdzToQ8F9vKQ8A",
+    authDomain: "xp-network-96e8f.firebaseapp.com",
+    projectId: "xp-network-96e8f",
+    storageBucket: "xp-network-96e8f.appspot.com",
+    messagingSenderId: "567445058390",
+    appId: "1:567445058390:web:57907617049c679e889cf7",
+    measurementId: "G-NZBQC708NM"
+  };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
